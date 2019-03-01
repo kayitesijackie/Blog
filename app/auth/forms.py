a3f97f40f2694e8f6873701bdddf1279e3d7aed5
kayitesijackie <bazimyas@gmail.com>
2019-03-01 18:40:38 +0200

    add RegistrationForm and LoginForm classes

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -20,8 +20,6 @@ class RegistrationForm(FlaskForm):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is already taken')
 
-
-
 class LoginForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
     password = PasswordField('Password',validators =[Required()])
