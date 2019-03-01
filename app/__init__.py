7c6925c99d8dd84e6b164091b6c1e69f4e6111a0
kayitesijackie <bazimyas@gmail.com>
2019-03-01 19:23:39 +0200

    import Flsk, Bootstrap, SQLALchemy and LoginManager

--- app/__init__.py
+++ app/__init__.py
@@ -25,6 +25,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    
     login_manager.init_app(app)
     mail.init_app(app)
 
